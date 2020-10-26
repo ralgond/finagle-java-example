1cd817e832441ed81b1939cd4d8f9c06c643bf12
ralgond <ht201806@outlook.com>
2020-10-26 10:23:56 +0800

    Another way to build a client.

--- src/main/java/com/github/ralgond/fje/UserServiceClient.java
+++ src/main/java/com/github/ralgond/fje/UserServiceClient.java
@@ -1,28 +1,62 @@
 package com.github.ralgond.fje;
 
+import org.apache.thrift.protocol.TBinaryProtocol;
+
 import com.github.ralgond.fje.User;
 import com.github.ralgond.fje.UserService;
+import com.twitter.finagle.ServiceFactory;
 import com.twitter.finagle.Thrift;
+import com.twitter.finagle.thrift.RichClientParam;
+import com.twitter.finagle.thrift.ThriftClientRequest;
 import com.twitter.util.Await;
-import com.twitter.util.Function;
 import com.twitter.util.Future;
 import com.twitter.util.TimeoutException;
 
-import scala.runtime.BoxedUnit;
-
 public class UserServiceClient {
 
-	public static void main(String args[]) throws TimeoutException, InterruptedException {
+	public static void client1() {
 		UserService.ServiceIface client = Thrift.client().build("localhost:5555", UserService.ServiceIface.class);
+
+		System.out.println(Thread.currentThread());
 		Future<User> resp = client.createUser("ht3");
-		resp.onSuccess(new Function<User, BoxedUnit>() {
-			@Override
-			public BoxedUnit apply(User v1) {
-				System.out.println(v1);
-				return null;
-			}
+		resp.onSuccess(v -> {
+			System.out.println(v);
+			System.out.println(Thread.currentThread());
+			return null;
 		});
-		
-		Await.ready(resp);
+
+		try {
+			Await.ready(resp);
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void client2() {
+		ServiceFactory<ThriftClientRequest, byte[]> serviceFactory = Thrift.client().newClient("localhost:5555");
+		UserService.ServiceToClient client = new UserService.ServiceToClient(serviceFactory.toService(),
+				RichClientParam.apply(new TBinaryProtocol.Factory()));
+
+		Future<User> resp = client.createUser("ht5");
+		resp.onSuccess(v -> {
+			System.out.println(v);
+			return null;
+		});
+
+		try {
+			Await.ready(resp);
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		serviceFactory.close();
+	}
+
+	public static void main(String args[]) {
+		client2();
 	}
 }
